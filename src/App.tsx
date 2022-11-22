7ab04ec3c19f0f59c5bce18bba1ac71b03f7cc51
LeoRouillard <leorouillard@gmail.com>
2022-11-22 11:46:10 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+function Calculatrice() {
+
+  let listButton = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  let [formule, setFormule] = useState('');
+  let [res, setRes] = useState(0)
+
+  let calculate = () => {
+    
+  }
+
+  return <div id='main'>
+    <div id='screen'>
+      { formule }
+    </div>
+    <div id='numbers'>
+      {listButton.map(n => <button onClick={ () => setFormule(formule+n) }>{n}</button>)}
+    </div>
+    <div id='operations'>
+      <button onClick={ () => setFormule(formule+'+') }>+</button>
+      <button onClick={ () => setFormule(formule+'-') }>-</button>
+      <button onClick={ () => setFormule(formule+'/') }>/</button>
+      <button onClick={ () => setFormule(formule+'*') }>*</button>
+    </div>
+    <div id='bouton'>
+      <button onClick={ () => calculate()}>=</button>
+      <button onClick={ () => setFormule('')}>C</button>
+    </div>
+  </div>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Calculatrice />
     </div>
   );
 }
